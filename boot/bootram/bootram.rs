bdd522d5c4b262144a1e12af155f72e994f17be6
Matteo Bertucci <matteobertucci2004@gmail.com>
2021-10-07 16:03:02 +0000

    Bootram: give it a 40KiB heap

--- boot/bootram/bootram.rs
+++ boot/bootram/bootram.rs
@@ -2,10 +2,23 @@
 #![no_main]
 #![feature(asm)]
 #![feature(panic_info_message)]
+#![feature(alloc_error_handler)]
 
+use core::alloc::Layout;
 use core::panic::PanicInfo;
+use buddy_system_allocator::LockedHeap;
+
+#[allow(improper_ctypes)]
+extern "C" {
+    static bootram_heap_start: ();
+    static bootram_heap_end: ();
+}
+extern crate alloc;
 
 mod bios_services;
+
+#[global_allocator]
+static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::empty();
 
 #[panic_handler]
 fn panic_handler(panic_info: &PanicInfo) -> ! {
@@ -29,9 +42,33 @@ fn panic_handler(panic_info: &PanicInfo) -> ! {
     loop {}
 }
 
+#[alloc_error_handler]
+fn alloc_error_handler(_: Layout) -> ! {
+    panic!("Error allocating memory")
+}
+
+fn init_allocator() {
+    unsafe {
+        let start = core::ptr::addr_of!(bootram_heap_start);
+        let end = core::ptr::addr_of!(bootram_heap_end);
+
+        HEAP_ALLOCATOR.lock().init(start as usize, end as usize - start as usize)
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn bootram_entry() -> ! {
     bios_services::print_string(b"Executing bootram...\n\r");
+    init_allocator();
+
+    let mut xs = alloc::vec::Vec::new();
+
+    xs.push(42);
+    if xs.pop() != Some(42) {
+        panic!("Some went wrong")
+    } else {
+        bios_services::print_string(b"Heap is working!\n\r")
+    }
 
     panic!("No boot sequence is defined yet");
 }
